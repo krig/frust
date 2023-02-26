5c5f94a023a8c5fdd21ad7dd55f84a94dfd471e6
Kristoffer Grönlund <krig@koru.se>
2023-02-26 01:13:02 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# frust
+
+Forth interpreter written in rust.
+
+Mainly a way for me to practice my keyboard layout writing some code.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use text_io::read;
+use std::io::*;
+
+
+
+fn interpret(stack: &mut Vec<i32>, line: String) {
+    for word in line.split_whitespace() {
+        if let Ok(num) = word.parse() {
+            stack.push(num);
+        } else {
+            match word {
+                "dup" => {
+                    stack.push(*stack.last().unwrap());
+                }
+                "+" => {
+                    let a = stack.pop().unwrap();
+                    let b = stack.pop().unwrap();
+                    stack.push(a + b);
+                }
+                "p" => {
+                    println!("{}", stack.last().unwrap());
+                }
+                _ => {
+                }
+            }
+        }
+        println!("{:?}", stack);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut stack = Vec::<i32>::new();
+    loop {
+        print!("> ");
+        let _ = stdout().flush();
+        let line: String = read!("{}\n");
+      
+        interpret(&mut stack, line);
+    }
 }
